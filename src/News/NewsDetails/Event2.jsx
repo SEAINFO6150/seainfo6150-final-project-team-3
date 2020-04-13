98ff369810b2af34a71eccee70300498836b334b
肖梓轩 <xiaozixuan@xiaozixuandeMacBook-Pro.local>
2020-04-13 16:14:54 -0700

    0413update

--- src/News/NewsDetails/Event2.jsx
+++ src/News/NewsDetails/Event2.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 import styles from "./NewsDetails.module.css";
-
+import Data from "../../data/NewsData.json";
 
 const Event2 = props => {
   return (
       <div className={styles.container}>
-          <ul className={styles.newsDetailsul}>
-             <li>
-                <img className={styles.newsImage} src= {"https://besthqwallpapers.com/img/original/65107/border-collie-brown-dog-pets-human-friend-cute-animals.jpg"} alt={"dog"} />
-             </li>
-             <li>
-                <h1>Holiday Giving Tree</h1>
-                <p>Homeward Pet’s Holiday Giving Tree is an opportunity for community members to help provide food and supplies for the care of the homeless dogs and cats that come through our doors.
-
-                   Hosting a Holiday Giving Tree at your business or collecting these gifts is a simple, creative, and tangible way to help out the animals in need in your community. Homeward Pet will provide hang-able tree ornaments with suggested items that help in the care of the dogs and cats in the shelter. You can display the tags as ornaments on your giving tree in a common space, or do something new and creative with them that inspires the holiday spirit.
-
-                   If you are interested in learning more or organizing a Holiday Giving Tree, please contact:
-
-                   Natasha Danzinger, Event Manager
-                   natasha@homewardpet.org
-                   425-488-4444 ext. 4015
-
-                    </p>
-             </li>
-          </ul>
-      </div>
+            <h1>{Data.EventsItemData.b.head}</h1>
+                <ul className={styles.newsDetailsul}>
+                   <li>
+                      <img className={styles.newsImage} src= {Data.EventsItemData.b.url} alt={"dogb"} />
+                      </li>
+                         <li>
+                      <p>{Data.EventsItemData.b.para}</p>
+                   </li>
+                </ul>
+            </div>
 
 
   );
--- src/News/NewsDetails/News3.jsx
+++ src/News/NewsDetails/News3.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import styles from "./NewsDetails.module.css";
-
+import Data from "../../data/NewsData.json";
 
 const News3 = props => {
   return (
       <div className={styles.container}>
-          <ul className={styles.newsDetailsul}>
-             <li>
-                <img className={styles.newsImage} src= {"https://besthqwallpapers.com/img/original/65107/border-collie-brown-dog-pets-human-friend-cute-animals.jpg"} alt={"dog"} />
-             </li>
-             <li>
-                <h1>Easter B’egg Hunt</h1>
-                <p>"In light of the ban on large events and social gatherings, we have decided
-                    to cancel our plans for the Easter B’egg Hunt on Saturday, April 4, 2020
-                    This event was to take place at the Willis Tucker Park off-leash dog area in Snohomish.
-                    Though we are disappointed, we have made this decision with safety as our top priority.
-                    We are so humbled and grateful to be part of such a generous and giving community. Since we have lost fundraising revenue from events for the near future, we are in need of extra support at this critical time to continue providing care for homeless animals. If you are able to support us during this tough time, we would greatly appreciate it."</p>
-             </li>
-          </ul>
-      </div>
+            <h1>{Data.NewsItemData.c.head}</h1>
+                <ul className={styles.newsDetailsul}>
+                   <li>
+                      <img className={styles.newsImage} src= {Data.NewsItemData.c.url} alt={"dogc"} />
+                      </li>
+                         <li>
+                      <p>{Data.NewsItemData.c.para}</p>
+                   </li>
+                </ul>
+            </div>
 
 
   );
