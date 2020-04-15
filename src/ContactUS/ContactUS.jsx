2d285cfd062efdd47fbb43a439e539c3257d573b
Tracy-Lei <leiyan0115@Yans-MacBook-Pro.local>
2020-04-15 02:21:28 -0700

    final version2

--- src/ContactUS/ContactUS.jsx
+++ src/ContactUS/ContactUS.jsx
@@ -11,16 +11,26 @@ const ContactUs = () => {
                    <li>
                       <div className={styles.contactInfo}>
                          <ul>
-                             <li><strong>Street Address</strong><br />
-                                    <p>13132 NE 177th Place<br />
-                                    Woodinville, WA  98072</p></li>
-                             <li><strong>Mailing Address</strong><br />
-                                    <p>P.O. Box 2293<br />
-                                    Woodinville, WA  98072</p></li>
-                             <li><strong>Phone</strong><br />
-                                    <p>(206)488.4444</p></li>
-                             <li><strong>Emailing Address</strong></li>
-                             <li><p><a href="info@homewardpet.org">info@homewardpet.org</a></p></li>
+                             <li>
+                                 <strong>Street Address:</strong><br/>
+                                <p>13132 NE 177th Place<br />
+                                    Woodinville, WA  98072
+                                </p>
+                             </li>
+                             <li>
+                                 <strong>Mailing Address:</strong><br/>
+                                 <p>P.O. Box 2293<br />
+                                    Woodinville, WA  98072
+                                 </p>
+                             </li>
+                             <li>
+                                 <strong>Phone:</strong><br/>
+                                 <p>(206)488.4444</p>
+                             </li>
+                             <li>
+                                 <strong>Emailing Address:</strong>
+                                 <p><a href="mailto: info@homewardpet.org">info@homewardpet.org</a></p>
+                             </li>
                          </ul>
                       </div>
                    </li>
